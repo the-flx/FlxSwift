1cce937c0959ae03935761cd7317739f47bf7557
JenChieh <jcs090218@gmail.com>
2024-12-29 15:03:24 -0800

    add incvec

--- Sources/Flx/Flx.swift
+++ Sources/Flx/Flx.swift
@@ -3,43 +3,57 @@
 
 class Flx {
     static let wordSeparators: [Character] = [" ", "-", "_", ":", ".", "/", "\\"]
-    
+
     static let defaultScore: Int = -35
-    
+
     /// Check if `ch` is a word character.
-    static func isWord(ch : Character?) -> Bool {
-        if (ch == nil) {
+    static func isWord(ch: Character?) -> Bool {
+        if ch == nil {
             return false
         }
         return !wordSeparators.contains(ch!)
     }
-    
+
     /// Check if` ch` is an uppercase character.
-    static func isCapital(ch : Character?) -> Bool {
+    static func isCapital(ch: Character?) -> Bool {
         return isWord(ch: ch) && String([ch!]) == ch?.uppercased()
     }
-    
+
     /// Check if `lastCh` is the end of a word and `ch` the start of the next.
     ///
     /// This function is camel-case aware.
-    static func isBoundary(lastCh : Character?, ch: Character) -> Bool {
-        if (lastCh == nil) {
+    static func isBoundary(lastCh: Character?, ch: Character) -> Bool {
+        if lastCh == nil {
             return true
         }
-        if (!isCapital(ch: lastCh) && isCapital(ch: ch)) {
+        if !isCapital(ch: lastCh) && isCapital(ch: ch) {
             return true
         }
-        if (!isWord(ch: lastCh) && isWord(ch: ch)) {
+        if !isWord(ch: lastCh) && isWord(ch: ch) {
             return true
         }
         return false
     }
-    
-    static func incVec() {
-        
+
+    /// Increment each element in `vec` between `beg` and `end` by `inc`.
+    static func incVec(vec: inout [Int], inc: Int?, beg: Int?, end: Int?) {
+        let _inc: Int = (inc == nil) ? 1 : inc!
+        var _beg: Int = (beg == nil) ? 0 : beg!
+        let _end: Int = (end == nil) ? vec.count : end!
+
+        while _beg < _end {
+            vec[_beg] += _inc
+            _beg += 1
+        }
     }
-    
-    public static func score() {
+
+    public class Result {
         // ..
+    }
+
+    /// Return best score matching `query` against `str`.
+    public static func score(str: String, query: String) -> Result? {
+        // ..
+        return nil
     }
 }
--- Tests/FlxTests/FlxTests.swift
+++ Tests/FlxTests/FlxTests.swift
@@ -1,4 +1,5 @@
 import Testing
+
 @testable import Flx
 
 @Test func testWord() async throws {
@@ -11,4 +12,10 @@ import Testing
     #expect(Flx.isCapital(ch: "C") == true)
     #expect(Flx.isCapital(ch: "c") == false)
     #expect(Flx.isCapital(ch: " ") == false)
+}
+
+@Test func testIncVec() async throws {
+    var arr: [Int] = [1, 2, 3]
+    Flx.incVec(vec: &arr, inc: 1, beg: 0, end: 3)
+    #expect(arr == [2, 3, 4])
 }
